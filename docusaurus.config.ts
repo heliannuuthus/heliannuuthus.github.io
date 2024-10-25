3a511406f6c379abb8df2a5afe9055733a2f6f60
heliannutuhus <heliannuthus@gmail.com>
2024-10-25 22:29:06 +0800

    format

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,33 +1,33 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-  title: 'heliannuuthus',
-  tagline: 'heliannuuthus',
-  favicon: 'img/favicon.ico',
+  title: "heliannuuthus",
+  tagline: "heliannuuthus",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://heliannuuthus.github.io/',
+  url: "https://heliannuuthus.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'heliannuuthus', // Usually your GitHub org/user name.
-  projectName: 'heliannuuthus.github.io', // Usually your repo name.
+  organizationName: "heliannuuthus", // Usually your GitHub org/user name.
+  projectName: "heliannuuthus.github.io", // Usually your repo name.
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   markdown: {
     mermaid: true,
@@ -37,7 +37,7 @@ const config: Config = {
 
       // Process front matter description placeholders
       result.frontMatter.description =
-        result.frontMatter.description?.replaceAll('{{MY_VAR}}', 'MY_VALUE');
+        result.frontMatter.description?.replaceAll("{{MY_VAR}}", "MY_VALUE");
 
       // Create your own front matter shortcut
       if (result.frontMatter.i_do_not_want_docs_pagination) {
@@ -56,27 +56,27 @@ const config: Config = {
   },
   presets: [
     [
-      'classic',
+      "classic",
       {
-        docs: false,  
+        docs: false,
         blog: {
-          routeBasePath: '/', 
+          routeBasePath: "/",
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -84,21 +84,21 @@ const config: Config = {
 
   themeConfig: {
     navbar: {
-      title: 'heliannuuthus',
+      title: "heliannuuthus",
       logo: {
-        alt: 'heliannuuthus',
-        src: 'img/logo.svg',
+        alt: "heliannuuthus",
+        src: "img/logo.svg",
       },
       items: [
         {
-          href: 'https://github.com/heliannuuthus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/heliannuuthus",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
     prism: {
