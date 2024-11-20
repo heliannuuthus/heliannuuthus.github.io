6ba1940aaeedaa88a7c87f2f07f0a8714aa49d90
heliannutuhus <heliannuthus@gmail.com>
2024-11-20 22:11:10 +0800

    build tree

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,5 +1,25 @@
 import { Steps as AntdSteps, StepsProps } from "antd";
+import { createStyles } from "antd-style";
+
 import { useState } from "react";
+
+const useStyles = createStyles(({ token, css }) => ({
+  container: css`
+    .ant-steps-item-wait {
+      .ant-steps-item-icon > .ant-steps-icon {
+        color: ${token.colorText
+          .replace("rgb", "rgba")
+          .replace(")", ", 0.45)")};
+      }
+      .ant-steps-item-container > .ant-steps-item-content > .ant-steps-item-title {
+        color: ${token.colorText
+          .replace("rgb", "rgba")
+          .replace(")", ", 0.45)")};
+      }
+    }
+  `,
+}));
+
 const Steps = ({ items, ...props }: StepsProps) => {
   const [current, setCurrent] = useState(0);
   const [showDescription, setShowDescription] = useState(false);
@@ -7,11 +27,13 @@ const Steps = ({ items, ...props }: StepsProps) => {
     setCurrent(value);
   };
 
+  const { styles } = useStyles();
+
   const stepsWithDescription: StepsProps["items"] = items?.map(
     (item, index) => ({
       ...item,
       description: index === current ? item.description : undefined,
-    }),
+    })
   );
 
   return (
@@ -20,6 +42,7 @@ const Steps = ({ items, ...props }: StepsProps) => {
       {...props}
       current={current}
       onChange={onChange}
+      className={styles.container}
     />
   );
 };
