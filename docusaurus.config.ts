2f50b8073e64b1bdd441f9f6c1ef1d13173a520a
heliannutuhus <heliannuthus@gmail.com>
2025-03-03 20:26:12 +0800

    color group

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://heliannuuthus.github.io/",
+  url: "https://site.heliannuuthus.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
